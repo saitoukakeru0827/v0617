344b89eefca40fef6439141018d4c13facec5dd0
A601_01 <tnk.dat.class@gmail.com>
2021-06-17 09:39:56 +0900

    kakuninn

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,24 @@ namespace v0617
 {
     public partial class Form1 : Form
     {
+        int vx = -10, vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label1.Left <= 0)
+                vx = -vx;
+            if (label1.Top <= 0)
+                vy = -vy;
+            if (label1.Left >= 665)
+                vx = -vx;
+            if (label1.Top >= 404)
+                vy = -vy;
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
